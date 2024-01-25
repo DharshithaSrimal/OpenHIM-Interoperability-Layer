d33194c07a65d9fb6a0cd5adbbbccd6f30a39154
Dharshitha Senevirathne <dharshithasrimal@gmail.com>
2024-01-25 22:04:02 +0530

    Update DO Perfomance Report.sql

--- scripts/SQL View/DO Perfomance Report.sql
+++ scripts/SQL View/DO Perfomance Report.sql
@@ -5,7 +5,8 @@ SELECT
     COUNT(DISTINCT CASE WHEN psi.eventdatavalues->'caq1Rf8wDx7'->>'value' = 'lowrisk' THEN psi.uid ELSE NULL END) AS CLIENTS_NOT_AT_RISK,
     COUNT(DISTINCT CASE WHEN psi.eventdatavalues->'igk7wHUjNoY'->>'value' = 'false' THEN psi.uid ELSE NULL END) AS CLIENTS_NOT_CONSENT_TO_SCREENING,
     COUNT(DISTINCT CASE WHEN psi.eventdatavalues->'H96DVMnlKbi'->>'value' IS NOT NULL THEN psi.uid ELSE NULL END) AS CLIENTS_REFERRED,
-    COUNT(DISTINCT CASE WHEN (psi.eventdatavalues != '{}' AND psi.eventdatavalues->'H96DVMnlKbi'->>'value' IS NULL) THEN psi.uid ELSE NULL END) AS CLIENTS_NOT_REFERRED
+    COUNT(DISTINCT CASE WHEN (psi.eventdatavalues != '{}' AND psi.eventdatavalues->'H96DVMnlKbi'->>'value' IS NULL) THEN psi.uid ELSE NULL END) AS CLIENTS_NOT_REFERRED,
+	COUNT(DISTINCT CASE WHEN (psi.eventdatavalues != '{}' AND psi.eventdatavalues->'H96DVMnlKbi'->>'value' IS NULL) THEN psi.uid ELSE NULL END) AS CLINIC_VISITS_CONFIRMED
 FROM
     trackedentityinstance AS tei
 INNER JOIN userinfo AS ui ON ui.username = tei.createdbyuserinfo->>'username'
