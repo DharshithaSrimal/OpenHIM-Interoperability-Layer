89a07e47dac0b4cdef287165508eb765a9648e89
Dharshitha Senevirathne <dharshithasrimal@gmail.com>
2025-01-05 16:27:49 +0100

    Update hospital_visits.py

--- scripts/hospital_visits.py
+++ scripts/hospital_visits.py
@@ -39,8 +39,8 @@ FOLLOWUP = config["FOLLOWUP"]
 tei_event_followup = ("Phone_Calls_Completed", "Home_Visits_Completed", "HLC_Visit_Completed")
 
 # Date range to loop over (2022-09-15 to 2022-09-30)
-start_date_range = datetime(2022, 9, 15)
-end_date_range = datetime(2022, 9, 30)
+start_date_range = datetime(2000, 1, 1)
+end_date_range = datetime(2020, 1, 1)
 
 # Safely extract the value with nested dictionaries and lists
 def get_nested_value(data, keys, default=None):
@@ -55,38 +55,47 @@ def get_nested_value(data, keys, default=None):
     return data
 
 # Loop over each day in the date range
-current_date = start_date_range
-while current_date <= end_date_range:
-    START_DATE = current_date.strftime("%Y-%m-%dT00:00:00Z")
-    END_DATE = (current_date + timedelta(days=1)).strftime("%Y-%m-%dT00:00:00Z")
-    print(f"Processing date range: {START_DATE} to {END_DATE}")
+# current_date = start_date_range
+# START_DATE = current_date.strftime("%Y-%m-%dT00:00:00Z")
+# END_DATE = (current_date + timedelta(days=1)).strftime("%Y-%m-%dT00:00:00Z")
+START_DATE = "2000-01-01T00:00:00"
+END_DATE = "2022-05-10T00:00:00"
+print(f"Processing date range: {START_DATE} to {END_DATE}")
     
-    # Get Keycloak token
-    token_response = requests.post(KEYCLOAK_URL, data={
+# Get Keycloak token
+token_response = requests.post(KEYCLOAK_URL, data={
         "client_id": CLIENT_ID,
         "client_secret": CLIENT_SECRET,
         "grant_type": GRANT_TYPE
-    })
-    ACCESS_TOKEN = token_response.json().get("access_token")
+})
+ACCESS_TOKEN = token_response.json().get("access_token")
 
-    # Encounters count
-    encounters = requests.get(
-    f"{FHIR_SERVER_URL}/fhir/Encounter?type=facility_visit&_count=0&_lastUpdated=ge{START_DATE}&_lastUpdated=le{END_DATE}",
-    headers={"Authorization": f"Bearer {ACCESS_TOKEN}"})
-    encounter_count = encounters.json().get("total")
+# Encounters count
+encounters = requests.get(
+f"{FHIR_SERVER_URL}/fhir/Encounter?type=facility_visit&_count=0&date=ge{START_DATE}&date=le{END_DATE}&_tag=PKT0010397",
+headers={"Authorization": f"Bearer {ACCESS_TOKEN}"})
+encounter_count = encounters.json().get("total")
+print("Count: ", encounter_count)
+# FHIR encounters
+encounter_response = requests.get(
+    f"{FHIR_SERVER_URL}/fhir/Encounter?type=facility_visit&date=ge{START_DATE}&date=le{END_DATE}&_count={encounter_count}&_tag=PKT0010397&_sort=date",
+    headers={"Authorization": f"Bearer {ACCESS_TOKEN}"}
+)
 
-    # FHIR encounters
-    encounter_response = requests.get(
-        f"{FHIR_SERVER_URL}/fhir/Encounter?type=facility_visit&_lastUpdated=ge{START_DATE}&_lastUpdated=le{END_DATE}&_sort=_lastUpdated&_count={encounter_count}",
-        headers={"Authorization": f"Bearer {ACCESS_TOKEN}"}
-    )
+encounter_response_data = encounter_response.json()
+encounter_response_bundle = encounter_response_data.get("entry", [])
 
-    encounter_response_data = encounter_response.json()
-    encounter_response_bundle = encounter_response_data.get("entry", [])
-
-    for encounter_info in encounter_response_bundle:
+for encounter_info in encounter_response_bundle:
         patient_id = encounter_info["resource"]["subject"]["reference"].split('/')[-1]
         print(patient_id)
+        
+        # Extract encounter date and set time range
+        encounter_start_date = encounter_info["resource"]["period"]["start"].split('T')[0].strip()
+        print(encounter_start_date)
+        encounter_date = f"{encounter_start_date}T00:00:00".strip()
+        encounter_dayend = f"{encounter_start_date}T23:59:59".strip()
+        print("start: ", encounter_date, " end: ", encounter_dayend)
+        # encounter_end = encounter_info["resource"]["period"]["end"].split('T')
         patient = None
         if patient_id:
             patient = requests.get(
@@ -102,7 +111,7 @@ while current_date <= end_date_range:
             'filter': UNIQUE_ID + ':eq:' + patient_id
         }
 
-        tei_id, org_unit = None, None
+        tei_id, org_unit,enrollment = "", "", ""
         try:
             tei_response = api.get('trackedEntityInstances', params=params)
             tei_response_json = tei_response.json()
@@ -111,7 +120,20 @@ while current_date <= end_date_range:
                 tei_id = tei_response_json['trackedEntityInstances'][0]['trackedEntityInstance']
                 org_unit = tei_response_json['trackedEntityInstances'][0]['orgUnit']
             else:
-                tei_id, org_unit = None, None
+                tei_id, org_unit = "", ""
+            if (tei_id):
+                # Fetch TEI
+                params = {
+                                'fields': 'trackedEntityInstance,enrollment,orgUnit',
+                                'ou': 'EF5pbiM7RSX',
+                                'trackedEntityInstance': tei_id
+                }
+                tei_enrollment = api.get('enrollments', params=params)
+                tei_enrollment_json = tei_enrollment.json()
+                # Get TEI and org unit id
+                if tei_enrollment_json['enrollments']:
+                    enrollment = tei_enrollment_json['enrollments'][0]['enrollment']
+                       
         except Exception as e:
             print(f"TEI Fetch Failed: {str(e)}")
 
@@ -120,12 +142,11 @@ while current_date <= end_date_range:
                 "resourceType": "Bundle",
                 "type": "batch",
                 "entry": [
-                    {"request": {"method": "GET", "url": f"Patient/{patient_id}"}},
-                    {"request": {"method": "GET", "url": f"Encounter?subject=Patient/{patient_id}&_lastUpdated=ge{START_DATE}&_lastUpdated=le{END_DATE}"}},
-                    {"request": {"method": "GET", "url": f"Observation?subject=Patient/{patient_id}&_lastUpdated=ge{START_DATE}&_lastUpdated=le{END_DATE}"}},
-                    {"request": {"method": "GET", "url": f"Condition?subject=Patient/{patient_id}&_lastUpdated=ge{START_DATE}&_lastUpdated=le{END_DATE}"}}
+                    {"request": {"method": "GET", "url": f"Observation?subject=Patient/{patient_id}&date=ge{encounter_date}&date=le{encounter_dayend}&_tag=PKT0010397&code=271062006,75367002"}},
+                    {"request": {"method": "GET", "url": f"Condition?subject=Patient/{patient_id}&_tag=PKT0010397"}}
                 ]
             }
+        
         # Send the POST request
         post_response = requests.post(
             f"{FHIR_SERVER_URL}/fhir",
@@ -135,121 +156,103 @@ while current_date <= end_date_range:
                 "Authorization": f"Bearer {ACCESS_TOKEN}"
             }
         )
-
         bundle = post_response.json()
-
         # Check if the response contains RiskAssessment resources and extract values
-        if "entry" in bundle:
-            for entry in bundle["entry"]:
-                # Extract RiskAssessment resource
-                if entry["resource"]["resourceType"] == "Encounter":
-                    encounter_resource = entry["resource"]
-                    event_date = encounter_resource.get("period").get("start")
+        if "entry" in bundle and tei_id and enrollment:
+            count = 0
+            for entry_ in bundle["entry"]:
+            
+                diabetes_present, hypertension_present = "false", "false"
+                blood_pressure_systolic, blood_pressure_diastolic, blood_sugar_value = "", "", ""
+                
+                if "entry" in entry_["resource"]:
+                    
+                    for entry in entry_["resource"]["entry"]:  
+                        if entry["resource"]["resourceType"] == "Condition":   
+                            condition_resource = entry.get("resource", {})
+                            condition_code = condition_resource.get("code", {}).get("coding", [{}])[0].get("code", "")
+                            if condition_code == "diabetes":
+                                diabetes_present = "true"
+                            if condition_code == "38341003" or condition_code == "hypertension":  # SNOMED code for hypertension
+                                hypertension_present = "true"
 
-                if entry["resource"]["resourceType"] == "Patient":   
-                    patient_response = entry["resource"] 
-                    screening_phn = None
-                    # Define the keys for the nested extraction
-                    screening_phn_keys = ["identifier", 0, "value"]
-                    # Extract the screening phn
-                    screening_phn = get_nested_value(patient_response, screening_phn_keys, default="N/A")
-                    params = {
-                        'fields': 'trackedEntityInstance,attributes[attribute,value]',
-                        'ouMode': 'ACCESSIBLE',
-                        'trackedEntityType': TRACKED_ENTITY_TYPE,
-                        'filter': TEI_ATTR_PHN + ':eq:' + screening_phn
-                    }
-                    tei_response = api.get('trackedEntityInstances', params=params)
-                    tei_response_json = tei_response.json()
+                        if entry["resource"]["resourceType"] == "Observation":   
+                            observation_resource = entry.get("resource", {})
+                            # Blood Sugar Value - you may need to adjust the code based on the LOINC code or system you're using
+                            blood_sugar_code = "271062006"  # LOINC code for blood glucose measurement, replace with relevant code
+                            for component in observation_resource.get("component", []):
+                                if component.get("code", {}).get("coding", [{}])[0].get("code") == blood_sugar_code:
+                                    blood_sugar_value = component.get("valueQuantity", {}).get("value")
+                            
+                            if observation_resource.get("code", {}).get("coding", [{}])[0].get("code") == blood_sugar_code:
+                                    blood_sugar_value = observation_resource.get("valueQuantity", {}).get("value")
+                            # Blood Pressure Value - usually involves systolic and diastolic measurements
+                            
+                            systolic_blood_pressure_code = "271649006"  # LOINC code for blood pressure, replace with relevant code
+                            diastolic_blood_pressure_code = "271650006"
+                            for component in observation_resource.get("component", []):
+                                if component.get("code", {}).get("coding", [{}])[0].get("code") == systolic_blood_pressure_code:
+                                    blood_pressure_systolic = component.get("valueQuantity", {}).get("value")
+                                if component.get("code", {}).get("coding", [{}])[0].get("code") == diastolic_blood_pressure_code:
+                                    blood_pressure_diastolic = component.get("valueQuantity", {}).get("value")
+                            if observation_resource.get("code", {}).get("coding", [{}])[0].get("code") == systolic_blood_pressure_code:
+                                    blood_pressure_systolic = observation_resource.get("valueQuantity", {}).get("value")
+                            if observation_resource.get("code", {}).get("coding", [{}])[0].get("code") == diastolic_blood_pressure_code:
+                                    blood_pressure_diastolic = observation_resource.get("valueQuantity", {}).get("value")
+                            # Output the results
+                            # if blood_sugar_value:
+                            #     print(f"Blood Sugar Value: {blood_sugar_value}")
+                            # if blood_pressure_systolic and blood_pressure_diastolic:
+                            #     print(f"Blood Pressure: {blood_pressure_systolic}/{blood_pressure_diastolic}")
 
-                    # Get TEI and org unit id
-                    if tei_response_json['trackedEntityInstances']:
-                        tei_id = tei_response_json['trackedEntityInstances'][0]['trackedEntityInstance']
-                        org_unit = tei_response_json['trackedEntityInstances'][0]['orgUnit']
-                    else:
-                        tei_id = ""
-                        org_unit = ""
+                        if (tei_id and enrollment):
+                            event_payload = {
+                                "events": [
+                                    {
+                                        "dataValues": [
+                                        {
+                                        "dataElement": "oipjzjylb5j",
+                                        "value": blood_pressure_diastolic
+                                        },
+                                        {
+                                        "dataElement": "lX3Mr4fDzt7",
+                                        "value": blood_sugar_value
+                                        },
+                                        {
+                                        "dataElement": "akdaaNwuAWT"
+                                        },
+                                        {
+                                        "dataElement": "ooWz0VBNypG",
+                                        "value": blood_pressure_systolic
+                                        },
+                                        {
+                                        "dataElement": "rW9y580Zer8",
+                                        "value": hypertension_present
+                                        },
+                                        {
+                                        "dataElement": "p1Jx0zlriQr",
+                                        "value": hypertension_present
+                                        }
+                                    ],
 
-                    if entry["resource"]["resourceType"] == "Condition": 
-                        diabetes_present = "false"
-                        hypertension_present = "false"  
-                        condition_keys = ["code", "coding", 0, "code"]
-                        # Extract the screening phn
-                        condition = get_nested_value(patient_response, screening_phn_keys, default="N/A")
-                        resource = entry.get("resource", {})
-                        condition_code = resource.get("code", {}).get("coding", [{}])[0].get("code", "")
-                        if condition_code == "diabetes":
-                            diabetes_present = "true"
-                        if condition_code == "38341003" or condition_code == "hypertension":  # SNOMED code for hypertension
-                            hypertension_present = "true"
+                                        "occurredAt": encounter_start_date,
+                                        "programStage": "Qpuicl4a94s",
+                                        "program": "jwn5nGdUepW",
+                                        "status": "COMPLETED",
+                                        "completedDate": encounter_start_date,
+                                        "orgUnit": "EF5pbiM7RSX",
+                                        "enrollment": enrollment,
+                                        "trackedEntity": tei_id
+                                    }
+                                ]
+                            }
+                                
+                            try:
+                                res = api.post('tracker', json=event_payload)
+                                print(f"Event creation successful", res.json())
+                            except Exception as e:
+                                print(f"Community screening event create failed: {str(e)}")
+                                     
+                                    
 
-                    if entry["resource"]["resourceType"] == "Observation":   
-                        patient_response = entry["resource"] 
-                        # Define the keys for the nested extraction
-                        screening_phn_keys = ["identifier", 0, "value"]
-                        # Extract the screening phn
-                        screening_phn = get_nested_value(patient_response, screening_phn_keys, default="N/A")
-                        # Extract the Practitioner Location code
-                        location_keys = ["meta", "tag", 4, "code"]
-                        location_code = get_nested_value(patient_response, location_keys, default="N/A")
-           
-        # Fetch TEI
-        params = {
-            'fields': 'trackedEntityInstance,attributes[attribute,value],orgUnit',
-            'ouMode': 'ACCESSIBLE',
-            'trackedEntityType': config[TRACKED_ENTITY_TYPE],
-            'filter': config[TEI_ATTR_PHN] + ':eq:' + screening_phn
-        }
-        tei_response = api.get('trackedEntityInstances', params=params)
-        tei_response_json = tei_response.json()
-
-        # Get TEI and org unit id
-        if tei_response_json['trackedEntityInstances']:
-            tei_id = tei_response_json['trackedEntityInstances'][0]['trackedEntityInstance']
-            org_unit = tei_response_json['trackedEntityInstances'][0]['orgUnit']
-        else:
-            tei_id = ""
-            org_unit = ""
-    # Move to the next day
-    current_date += timedelta(days=1)
-
-    payload = {
-      "dataValues": [
-        {
-          "dataElement": "oipjzjylb5j",
-          "value": "76"
-        },
-        {
-          "dataElement": "wuWLanPwym8",
-          "value": "Losartan"
-        },
-        {
-          "dataElement": "lX3Mr4fDzt7",
-          "value": "94"
-        },
-        {
-          "dataElement": "akdaaNwuAWT"
-        },
-        {
-          "dataElement": "ooWz0VBNypG",
-          "value": "144"
-        },
-        {
-          "dataElement": "rW9y580Zer8",
-          "value": "true"
-        },
-        {
-          "dataElement": "p1Jx0zlriQr",
-          "value": "false"
-        }
-      ],
-      "eventDate": "2023-10-10",
-      "programStage": "Qpuicl4a94s",
-      "program": "jwn5nGdUepW",
-      "event": "NxhU6lno2tP",
-      "status": "COMPLETED",
-      "completedDate": "2023-10-10",
-      "attributeOptionCombo": "uGIJ6IdkP7Q",
-      "orgUnit": "WoFlqQmqtSR",
-      "trackedEntityInstance": "yFtIAkcrZrp"
-    }
+    
